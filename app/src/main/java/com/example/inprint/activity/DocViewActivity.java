12bbc681981063a3a55682ce58ce5a91099c5e00
Wenpupil <951230509@qq.com>
2019-09-28 18:47:16 +0800

    UI优化 -- 版本变动 unstable

--- app/src/main/java/com/example/inprint/activity/DocViewActivity.java
+++ app/src/main/java/com/example/inprint/activity/DocViewActivity.java
@@ -9,38 +9,22 @@ import android.webkit.WebView;
 
 import com.example.inprint.R;
 import com.example.inprint.util.LogUtil;
+import com.example.inprint.util.StatusBarUtil;
+import com.example.inprint.util.WordUtil;
+import com.google.android.material.appbar.AppBarLayout;
 
-import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.converter.PicturesManager;
-import org.apache.poi.hwpf.converter.WordToHtmlConverter;
-import org.apache.poi.hwpf.usermodel.Picture;
-import org.apache.poi.hwpf.usermodel.PictureType;
 import org.apache.poi.xwpf.converter.core.FileImageExtractor;
 import org.apache.poi.xwpf.converter.core.FileURIResolver;
 import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
 import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.w3c.dom.Document;
-
-import java.io.BufferedWriter;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 /**
  * 查看文档
@@ -49,120 +33,63 @@ public class DocViewActivity extends AppCompatActivity {
     //创建生成的文件地址
     private static String docName = "123.docx";
     private static String docPath="/storage/emulated/0/";
+    private int rate;  //0表示docx，1表示doc
+
     private static final String savePath="/data/user/0/com.example.inprint/files/";
     private static final String tempPath="/data/user/0/com.example.inprint/files/";
+
+    private WordUtil wu;
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_view);
         intentData();
-        String name = docName.substring(0, docName.indexOf("."));
-        try {
-            if(!(new File(savePath+name).exists()))
-                new File(savePath+name).mkdirs();
-            LogUtil.d("fileView","找到文档");
-            docx2Html(docPath+docName,savePath+name+".html");
-            //convert2Html(docPath+docName,savePath+name+".html");
-        } catch (Exception e){
-            LogUtil.d("fileView","没有找到文档");
-            e.printStackTrace();
-        }
-        //WebView加载显示本地html文件
-        WebView webView = (WebView) this.findViewById(R.id.office);
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setSupportZoom(true);
-        webSettings.setBuiltInZoomControls(true);
-        webView.loadUrl("file://"+savePath+name+".html");
+        displayFile();
     }
     //获取启用此活动的数据
     private void intentData(){
+        StatusBarUtil.hideStatusBar(this);
         Intent intent=getIntent();
+        AppBarLayout appBarLayout = findViewById(R.id.al_actionbar);
         docPath=intent.getStringExtra("docUrl");
         docName=intent.getStringExtra("docName");
-    }
-    /**
-     * word文档转成html格式
-     * */
-    public void convert2Html(String fileName, String outPutFile)
-            throws TransformerException, IOException,
-            ParserConfigurationException {
-        HWPFDocument wordDocument = null;
-        try {
-            wordDocument = new HWPFDocument(new FileInputStream(fileName));
-            WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(
-                    DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
-            //设置图片路径
-            wordToHtmlConverter.setPicturesManager(new PicturesManager() {
-                public String savePicture(byte[] content,
-                                          PictureType pictureType, String suggestedName,
-                                          float widthInches, float heightInches) {
-                    String name = docName.substring(0, docName.indexOf("."));
-                    return name + "/" + suggestedName;
-                }
-            });
-            //保存图片
-            List<Picture> pics=wordDocument.getPicturesTable().getAllPictures();
-            if(pics!=null){
-                for(int i=0;i<pics.size();i++){
-                    Picture pic = (Picture)pics.get(i);
-                    System.out.println( pic.suggestFullFileName());
-                    try {
-                        String name = docName.substring(0,docName.indexOf("."));
-                        String file = savePath+ name + "/"
-                                + pic.suggestFullFileName();
-                        pic.writeImageContent(new FileOutputStream(file));
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            wordToHtmlConverter.processDocument(wordDocument);
-            Document htmlDocument = wordToHtmlConverter.getDocument();
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            DOMSource domSource = new DOMSource(htmlDocument);
-            StreamResult streamResult = new StreamResult(out);
 
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer serializer = tf.newTransformer();
-            serializer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
-            serializer.setOutputProperty(OutputKeys.INDENT, "yes");
-            serializer.setOutputProperty(OutputKeys.METHOD, "html");
-            serializer.transform(domSource, streamResult);
-            out.close();
-            //保存html文件
-            writeFile(new String(out.toByteArray()), outPutFile);
-        } catch (Exception e) {
-            e.printStackTrace();
+        //WebView加载显示本地html文件
+        webView = (WebView) this.findViewById(R.id.office);
+        WebSettings settings = webView.getSettings();
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setSupportZoom(true);
+        settings.setBuiltInZoomControls(true);// 设置WebView可触摸放大缩小
+        settings.setUseWideViewPort(true);
+
+        String rates = docName.substring(docName.lastIndexOf('.')+1);
+        if(rates.equals("docx")){
+            rate = 0;
+        }else{
+            rate = 1;
         }
     }
-
-    /**
-     * 将html文件保存到sd卡
-     * */
-    public void writeFile(String content, String path) {
-        FileOutputStream fos = null;
-        BufferedWriter bw = null;
-        try {
-            File file = new File(path);
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            fos = new FileOutputStream(file);
-            bw = new BufferedWriter(new OutputStreamWriter(fos,"utf-8"));
-            bw.write(content);
-        } catch (FileNotFoundException fnfe) {
-            fnfe.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } finally {
+    private void displayFile()
+    {
+        String name = docName.substring(0, docName.indexOf("."));
+        if(rate == 0) {
             try {
-                if (bw != null)
-                    bw.close();
-                if (fos != null)
-                    fos.close();
-            } catch (IOException ie) {
+                if (!(new File(savePath + name).exists()))
+                    new File(savePath + name).mkdirs();
+                LogUtil.d("性能分析", "开始解析文档");
+                docx2Html(docPath + docName, savePath + name + ".html");
+                LogUtil.d("性能分析", "文档解析完毕");
+            } catch (Exception e) {
+                LogUtil.d("fileView", "没有找到文档");
+                e.printStackTrace();
             }
+            webView.loadUrl("file://" + savePath + name + ".html");
+        }else{
+            String path = docPath + docName;
+            wu = new WordUtil(path);
+            webView.loadUrl("file://" + wu.htmlPath);
         }
     }
     /**
